f6edf28356ffb9505b56c164bd279bbd06aab7e6
wallace <wallystudyhard@gmail.com>
2022-01-20 01:46:47 -0300

    RVRent Agency Spring boot test

--- Sec-25-Spring-Boot/demo/src/main/java/com/rvrent/agency/DemoApplication.java
+++ Sec-25-Spring-Boot/demo/src/main/java/com/rvrent/agency/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RvrentApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RvrentApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
